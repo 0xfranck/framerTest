f798828e124361a95a5227d5e65e8cffdb01ce0c
0xfranck <105662496+0xfranck@users.noreply.github.com>
2025-03-24 07:24:44 +0000

    new

--- api/dynamic.js
+++ api/dynamic.js
@@ -1,5 +1,5 @@
-export async function onRequest(context) {
-    const content = await context.env.MY_KV.get("FramerFirst");
+  export async function onRequest(context) {
+    const content = await context.env.MY_KV.get("daily-page");
     return new Response(content || "No content available", {
       headers: { "Content-Type": "text/html" },
     });
