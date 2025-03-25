c0976a0fc30730bdae435080c0d6c01e507f23df
0xfranck <105662496+0xfranck@users.noreply.github.com>
2025-03-25 21:56:05 +0000

    new

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,18 @@
-export async function onRequest(context) {
-    const content = await context.env.MY_KV.get("daily-page");
-    const html = content || "<h1>No content available</h1>";
-    return new Response(html, {
-      headers: { "Content-Type": "text/html" },
-    });
-  }
+export default {
+    async fetch(request, env, ctx) {
+      try {
+        const targetUrl = "https://left-day-437461.framer.app/";
+        const response = await fetch(targetUrl);
+        const content = await response.text();
+  
+  
+        await env.MY_KV.put("FramerFirst", content);
+  
+        return new Response("Successful write", {
+          status: 201,
+        });
+      } catch (e) {
+        return new Response(e.message, { status: 500 });
+      }
+    },
+  };
